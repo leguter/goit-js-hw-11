1c59bb64d706c3da55ec96003e8208c58b6bff75
leguter <kolalevcenko80@gmail.com>
2024-06-17 12:51:18 -1200

    6

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,5 @@
+import iziToast from "izitoast";
+ import "izitoast/dist/css/iziToast.min.css";
 export function searchImg(img) {
     const baseUrl = 'https://pixabay.com';
     const endPoint = '/api/'
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,6 +1,18 @@
-function createImg() {
-`<li><img </li>`
-}
-function createImgs() {
+import { searchImg } from "./pixabay-api"
+searchImg().then(data => {
+    const markup = imgsTemplate(data)
+    
+}) 
 
+function imgTemplate({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+    return `<li>
+    <img src="${webformatURL}" alt="${tags}" data-source="${largeImageURL}"/>
+    <span>likes ${likes}</span>
+    <span>views ${views}</span>
+    <span>comments ${comments}</span>
+    <span>downloads ${downloads}</span>
+    </li>`
+}
+function imgsTemplate(arr) {
+return arr.map(imgTemplate).join('');
 }
